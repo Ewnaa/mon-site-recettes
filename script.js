95f233c61867abb12f09d5f1cfbbefdb5000369a
Ewnaa <graglia.ewan@gmail.com>
2025-04-18 18:19:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const recipes = [
   {
     title: "Salade César",
     category: "Entrée",
-    image: "https://source.unsplash.com/400x300/?salad",
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hellofresh.fr%2Frecipes%2Fsalade-cesar-de-luxe-au-poulet-5ba8a97130006c3be559c7d2&psig=AOvVaw3_mrq_0EWc5PCw-5LKjT77&ust=1745079345587000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLjnzqf94YwDFQAAAAAdAAAAABAE",
     description: "Une salade classique avec poulet, croûtons et parmesan."
   },
   {
     title: "Spaghetti Carbonara",
     category: "Plat",
-    image: "https://source.unsplash.com/400x300/?pasta",
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.marthastewart.com%2F338686%2Fspaghetti-carbonara&psig=AOvVaw0n19mIMeePAI-ra6GUJTGa&ust=1745079229879000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCLDY8e_84YwDFQAAAAAdAAAAABAE",
     description: "Des pâtes crémeuses avec lardons et œufs."
   },
   {
     title: "Tarte aux Fraises",
     category: "Dessert",
-    image: "https://source.unsplash.com/400x300/?strawberry-cake",
+    image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.patisserie-et-gourmandise.com%2Frecette%2Ftarte-fraise%2F&psig=AOvVaw1hSdVXzP0giRZskvqeb6MQ&ust=1745079385766000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOjlurr94YwDFQAAAAAdAAAAABAl",
     description: "Une tarte fraîche avec crème pâtissière et fraises."
   },
 ];
